f64fe5ea54aa48fa348cfb7bf8b07b421466e873
JoseCisneros <j.cisneros1195@gmail.com>
2019-12-20 17:12:39 -0600

    ReactDom.render

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
+// const element = document.createElement("h1");
+// element.innerText = "Hello, Platzi Badges!";
+// const container = document.getElementById("app");
 
 // container.appendChild(element);
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
 const element = <h1>Hello, Platzi Badges!</h1>;
 
-const container = document.getElementById('app');
-
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+const container = document.getElementById("app");
+ReactDom.render(element, container);
