0d0d5af297c66a1bb293b308acf48dcbbe7c7b5d
JoseCisneros <j.cisneros1195@gmail.com>
2019-12-25 10:44:09 -0600

    JSX

--- src/index.js
+++ src/index.js
@@ -4,10 +4,37 @@
 
 // container.appendChild(element);
 
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement(
+//   "a",
+//   { href: "https://platzi.com" },
+//   "Ir a Platzi"
+//   );
+// const name = 'Jose'
+// const element = React.createElement(
+//   'h1',
+//   {},
+//   `Hola, soy ${name}`
+// )
+// const sum = () => 3+3;
+// const jsx = <h1>Hola, soy {__expresiones__}</h1>
 
+const jsx = (
+  <div>
+    <h1>Hola soy jose</h1>
+    <p>Soy ingeniero frontend.</p>
+  </div>
+);
+
+const element = React.createElement(
+  'div',
+  {},
+  React.createElement('h1', {}, 'Hola, Soy Jose'),
+  React.createElement('p', {}, 'Soy ingeniero frontEnd')
+)
 const container = document.getElementById("app");
+
 ReactDom.render(element, container);
